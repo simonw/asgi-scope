e3af9c7fcc43645d6f07e8a43340b122d23aeca8
Simon Willison <swillison@gmail.com>
2020-04-08 17:36:26 -0700

    Upgrade to ASGI 2.0 - app(scope, receive, send) - refs #1

--- app.py
+++ app.py
@@ -1,21 +1,14 @@
 from pprint import pformat
 
 
-class App():
-    def __init__(self, scope):
-        self.scope = scope
-        if scope['type'] != 'http':
-            raise Exception()
-
-    async def __call__(self, receive, send):
-        await send({
-            'type': 'http.response.start',
-            'status': 200,
-            'headers': [
-                [b'content-type', b'text/plain'],
-            ],
-        })
-        await send({
-            'type': 'http.response.body',
-            'body': pformat(self.scope).encode('utf8'),
-        })
+async def app(scope, receive, send):
+    await send(
+        {
+            "type": "http.response.start",
+            "status": 200,
+            "headers": [[b"content-type", b"text/plain"],],
+        }
+    )
+    await send(
+        {"type": "http.response.body", "body": pformat(scope).encode("utf8"),}
+    )
